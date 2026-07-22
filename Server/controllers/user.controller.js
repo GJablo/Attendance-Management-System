db60e243df3434b6b2763b7ac2fe56f688a7c15d
deCipher <decipher@kali.kali>
2026-07-22 13:31:18 +0300

    Authorize middleware and user controller

---
+++ Server/controllers/user.controller.js
@@ -0,0 +1,40 @@
+import User from "../models/User.js";
+
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ success: true, data: users });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+
+    if (!user) {
+      const error = new Error("User not Found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not Found" });
+    }
+    res
+      .status(200)
+      .json({ success: true, message: "User deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
