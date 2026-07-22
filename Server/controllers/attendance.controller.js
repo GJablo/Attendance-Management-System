77806d1a668529051793fc84d6124013566b8307
deCipher <decipher@kali.kali>
2026-07-22 14:34:27 +0300

    Implemented getUserAttendanceLogic

--- Server/controllers/attendance.controller.js
+++ Server/controllers/attendance.controller.js
@@ -13,3 +13,25 @@ export const markAttendance = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getAttendances = async (req, res, next) => {
+  try {
+    const attendances = await Attendance.find();
+    res.status(200).json({ success: true, data: attendances });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserAttendance = async (req, res, next) => {
+  try {
+    // check if user matches token
+    if (req.user.id !== req.params.id) {
+      return res.status(403).json({ message: "Access denied! Not owner" });
+    }
+    const userAttendance = await Attendance.find({ user: req.params.id });
+    res.status(200).json({ success: true, data: userAttendance });
+  } catch (error) {
+    next(error);
+  }
+};
--- Server/routes/attendance.routes.js
+++ Server/routes/attendance.routes.js
@@ -1,16 +1,19 @@
 import { Router } from "express";
-import { markAttendance } from "../controllers/attendance.controller.js";
-import authorize from "../middlewares/auth.middleware.js";
+import {
+  getAttendances,
+  getUserAttendance,
+  markAttendance,
+} from "../controllers/attendance.controller.js";
+import authorize, { isAdmin } from "../middlewares/auth.middleware.js";
 
 const attendanceRouter = Router();
 
 attendanceRouter.post("/mark", authorize, markAttendance);
-attendanceRouter.get("/", async (req, res) => {
-  res.send({ message: "Getting attendance" });
-});
-attendanceRouter.get("/:id", async (req, res) => {
+attendanceRouter.get("/", authorize, isAdmin, getAttendances);
+attendanceRouter.get("/:id", authorize, isAdmin, async (req, res) => {
   res.send({ message: "Getting single attendance" });
 });
+attendanceRouter.get("/user/:id", authorize, getUserAttendance);
 attendanceRouter.put("/:id", async (req, res) => {
   res.send({ message: "Update single attendance" });
 });
