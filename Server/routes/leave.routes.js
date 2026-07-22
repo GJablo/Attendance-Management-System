ac7e3a7d08a87e239beb08ae9dd1231256cf951c
deCipher <decipher@kali.kali>
2026-07-22 16:18:21 +0300

    Implemented getLeave and updateLeave logic

--- Server/routes/leave.routes.js
+++ Server/routes/leave.routes.js
@@ -1,18 +1,20 @@
 import { Router } from "express";
-import { createLeaveRequest } from "../controllers/leave.controller.js";
-import authorize from "../middlewares/auth.middleware.js";
+import {
+  createLeaveRequest,
+  getLeaves,
+  updateLeave,
+} from "../controllers/leave.controller.js";
+import authorize, { isAdmin } from "../middlewares/auth.middleware.js";
 
 const leaveRouter = Router();
 
 leaveRouter.post("/", authorize, createLeaveRequest);
-leaveRouter.get("/", async (req, res) => {
-  res.send({ message: "Get employee leaves" });
-});
-leaveRouter.put("/:id", async (req, res) => {
-  res.send({ message: "Update a leave" });
-});
+leaveRouter.get("/", authorize, isAdmin, getLeaves);
+leaveRouter.put("/:id", authorize, isAdmin, updateLeave);
 leaveRouter.delete("/:id", async (req, res) => {
   res.send({ message: "Delete a leave" });
 });
+
+// cancel leave by requested user
 
 export default leaveRouter;
