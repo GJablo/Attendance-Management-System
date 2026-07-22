6b7959c3fe118e7b3932ee277b3af0e9f9b7ccd5
deCipher <decipher@kali.kali>
2026-07-22 17:14:24 +0300

    Implemented rate limiting using arcjet

--- Server/app.js
+++ Server/app.js
@@ -8,10 +8,15 @@ import reportRouter from "./routes/reports.routes.js";
 import leaveRouter from "./routes/leave.routes.js";
 import connectDB from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
