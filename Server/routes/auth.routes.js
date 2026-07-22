9b6c13148674904e33d108d68e88b41542a43747
deCipher <decipher@kali.kali>
2026-07-22 12:37:49 +0300

    Implemented logOut logic

--- Server/routes/auth.routes.js
+++ Server/routes/auth.routes.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { login, logOut, register } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
-authRouter.post("/logout", async (req, res) => {
-  res.send({ message: "Logout controller" });
-});
+authRouter.post("/logout", logOut);
 
 export default authRouter;
